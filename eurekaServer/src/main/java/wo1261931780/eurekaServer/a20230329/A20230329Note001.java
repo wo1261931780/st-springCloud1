6ec0b2c159d14afe352a83887cd89844d01e0dab
junw <45444154+wo1261931780@users.noreply.github.com>
2023-03-29 15:07:20 +0800

    day001-10

---
+++ eurekaServer/src/main/java/wo1261931780/eurekaServer/a20230329/A20230329Note001.java
@@ -0,0 +1,16 @@
+package wo1261931780.eurekaServer.a20230329;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-springCloud
+ * Package:wo1261931780.eurekaServer.a20230329
+ *
+ * @author liujiajun_junw
+ * @Date 2023-03-15-04  星期三
+ * @description
+ */
+public class A20230329Note001 {
+	//我们进入eureka注册中心的时候，就可以看到服务的状态
+	//如果是挂起的状态，就是up，服务运行中
+	//否则就是down，服务挂了
+}
